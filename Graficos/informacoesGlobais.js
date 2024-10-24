1f72bd88a7778ee0a808b86d65c4904698a7d4f3
nimsay <113374944+nimsaylindoca@users.noreply.github.com>
2024-10-24 11:33:52 +0000

    10

--- Graficos/informacoesGlobais.js
+++ Graficos/informacoesGlobais.js
@@ -1,7 +1,9 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarinformacoesGlobais() {
+async function vizualizarinformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
 }
+
+vizualizarinformacoesGlobais()
