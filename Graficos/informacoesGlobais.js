48befcf3d3ec9acf1993dd1c0a69c08c34325346
nimsay <113374944+nimsaylindoca@users.noreply.github.com>
2024-10-24 11:30:07 +0000

    24/10

--- Graficos/informacoesGlobais.js
+++ Graficos/informacoesGlobais.js
@@ -1,4 +1,7 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-
-console.log(url);
+async function visualizarinformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
